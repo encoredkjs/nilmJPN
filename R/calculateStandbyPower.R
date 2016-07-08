006118dfa8bf5ed6f6b25cb8f5e480c8f1b1ab9a
Jin-Sung Kim <kjs@encoredtech.com>
2016-07-08 10:02:02 +0900

    stable version

--- R/calculateStandbyPower.R
+++
@@ -1,2 +0,0 @@
-calculateStandbyPower <- function(data)
-  min(envelopeDetector(data)$UpperEnvelope) * length(data$active_power)
--- R/zoro.R
+++
@@ -1,7 +0,0 @@
-.onAttach <- function(libname, pkgname) {
-  ver <- read.dcf(file = system.file("DESCRIPTION", package = pkgname),
-                  fields = "Version")
-  packageStartupMessage("")
-  packageStartupMessage("Package ", pkgname, " (", ver, ") loaded.")
-  packageStartupMessage("")
-}
